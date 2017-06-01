4fa304332edef09c30ff2beaf23e7818b2485705
Wang Hankun <hakuwannng@qq.com>
2017-06-02 00:56:35 +0800

    added auto_upd.sh

---
+++ auto_upd.sh
@@ -0,0 +1,8 @@
+cp ~/.zshrc ~/Work/iTerm2-zsh-configuration-steps/config-files/
+cp ~/.vimrc ~/Work/iTerm2-zsh-configuration-steps/config-files/
+cp ~/.bash_profile ~/Work/iTerm2-zsh-configuration-steps/config-files/
+cd ~/Work/iTerm2-zsh-configuration-steps/config-files/
+git add .zshrc
+git add .vimrc
+git add .bash_profile
+cd ~/Work
